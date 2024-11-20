795ed87e34d6e58bf70ef710e75d4e867becd82d
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-11-20 11:08:56 -0300

    Usando database

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,31 @@ import { db } from "./firebaseConnection";
 
 
 function App() {
+
+function handleAdd(){
+  alert('TESTE')
+}
+
   return (
     <div >
     <h1>  React e Firebase..</h1>
+
+    <div>
+      <label>Titulo:</label>
+      <textarea 
+      type="text"
+      placeholder="Digite o titulo"
+      />
+
+      <label>Autor:</label>
+      <input 
+      type="text"
+      placeholder="Autor do post"
+      />
+
+      <button onClick={handleAdd}>Cadastrar</button>
+
+    </div>
     </div>
   );
 }
