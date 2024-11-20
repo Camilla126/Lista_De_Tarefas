454904814627ea7215fb9e08c23c3ffc7dcf952c
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-11-20 13:50:23 -0300

    armazenando dados nas useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react'
 import { db } from "./firebaseConnection";
+import "./app.css";
 
 
 function App() {
+
+const [titulo, setTitulo] = useState('');
+const [autor, setAutor] = useState('');
+
 
 function handleAdd(){
   alert('TESTE')
@@ -11,17 +17,21 @@ function handleAdd(){
     <div >
     <h1>  React e Firebase..</h1>
 
-    <div>
+    <div className="container">
       <label>Titulo:</label>
       <textarea 
       type="text"
       placeholder="Digite o titulo"
+      value={titulo}
+      onChange={(e)=> setTitulo(e.target.value)}
       />
 
       <label>Autor:</label>
       <input 
       type="text"
       placeholder="Autor do post"
+      value={autor}
+      onChange={(e) => setAutor(e.target.value)}
       />
 
       <button onClick={handleAdd}>Cadastrar</button>
