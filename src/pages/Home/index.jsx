5f646f072eed7e9837c5502d306db0c71eed7ece
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-12-04 09:17:59 -0300

    fazendo parte funcional da home e montando estrutura

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react"
+import '.home.css'
+
 export default function Home(){
+const [email, setEmail] = useState('')
+const [password, setPassword ] = useState('')
+
     return(
-      <div>
-      <h1>Página appp </h1>
-        
-        </div>
+      <div className="home-container">
+        <h1>Lista de tarefas</h1>
+        <span>Gerencie sua agenda de forma fácil.</span>
+    
+    <form>
+      <input 
+      type="text"
+      placeholder="Digite seu email..."
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+       />
+
+<input 
+autoComplete="false"
+      type="password"
+      placeholder="******"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+       />
+
+       <button type="submit">Acessar</button>
+    </form>
+      </div>
     )
   }
