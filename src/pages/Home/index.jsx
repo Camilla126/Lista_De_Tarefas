5edbd165df2003a3fc5ba3a0456b98b56aeb0cc8
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-12-04 09:39:15 -0300

    Criando função para submissão do botão login

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,12 +7,27 @@ export default function Home(){
 const [email, setEmail] = useState('')
 const [password, setPassword ] = useState('')
 
+function handleLogin(e){
+  e.preventDefault();
+
+if(email !== '' && password !== ''){
+ alert('TESTE')
+
+}else{
+    alert('Preencha todos os campos!')
+}
+ 
+
+
+
+}
+
     return(
       <div className="home-container">
         <h1>Lista de tarefas</h1>
         <span>Gerencie sua agenda de forma fácil.</span>
     
-    <form className="form">
+    <form className="form" onSubmit={handleLogin}>
       <input 
       type="text"
       placeholder="Digite seu email..."
