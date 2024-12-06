0bae52039d516196c3f09e357f25fddbaa7622bb
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-12-06 11:12:40 -0300

    controle de usuário feito

--- src/routes/private.jsx
+++ src/routes/private.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { auth } from '../firebaseConnection'
 import { onAuthStateChanged } from "firebase/auth";
 
+import { Navigate } from "react-router-dom";
+
 export default function Private ({ children }){
 const [loading, setLoading] = useState(true)
 const [signed, setSigned] = useState(false)
@@ -27,8 +29,18 @@ setSigned(true);
     })
     
 }
-
+checkLogin()
 }, [])
+
+if(loading){
+    return(
+        <div></div>
+    )
+}
+
+if(!signed){
+return <Navigate to="/"/>
+}
 
     return children;
 }
